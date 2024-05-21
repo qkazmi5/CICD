f99efee968406194a6e8fe370239b7d4a11af24f
unknown <kaz_fz@hotmail.com>
2024-05-21 19:12:00 +0500

    ADDED Commit

---
+++ scripts/start_container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+echo
+
+docker login -u "kazmi5" -p "321Softbiz@345"
+docker pull "kazmi5/simple-python-flask-app:latest" .
+docker run "kazmi5/simple-python-flask-app:latest"
+
+# Run the Docker image as a container
+echo
